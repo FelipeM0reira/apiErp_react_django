7eb07ff9799d2753e9f22ae66c3d39a2bf5eb42b
Felipe Moreira <felipeemooreira@gmail.com>
2026-01-13 22:02:06 -0300

    feat: accounts.models

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, Permission
 
+from companies.models import Enterprise
+
 class User(AbstractBaseUser):
     name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
@@ -8,8 +10,17 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.email
 
 class Group(models.Model):
-    name = models.CharField(max_Length=85)
+    name = models.CharField(max_length=85)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    
+class GroupPermission(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    
+class UserGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)

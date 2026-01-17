d7cc5fc354a3fa8fb5f4f9f4f9a9715f7c71e90d
Felipe <felipeemooreira@gmail.com>
2026-01-17 01:53:33 -0300

    Done add endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,11 @@
+from accounts.views.signin import Signin
+from accounts.views.signup import Singnup
+from accounts.views.user import GetUser
+
+from django.urls import path
+
 urlpatterns = [
-  
+    path('signin', Signin.as_view()),
+    path('signup', Singnup.as_view()),
+    path('user', GetUser.as_view())
 ]

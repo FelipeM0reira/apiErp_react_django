cd773a891d6b812239b430923164a78244e8db9e
Felipe <felipeemooreira@gmail.com>
2026-01-23 21:52:28 -0300

    feat(api): iniciar estrutura do serializer de usuários

---
+++ companies/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+from companies.models import Employee, Task
+from accounts.models import User_Groups, User, Group, Group_Permissions
+from django.contrib.auth.models import Permission

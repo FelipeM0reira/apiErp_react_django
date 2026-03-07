175bca0575fb8fb05d5ded03d8903a9f3bfe9459
Felipe <felipeemooreira@gmail.com>
2026-03-07 20:42:53 -0300

    feat: implement GET method in PermissionsDetail to retrieve all permissions

--- companies/views/permissions.py
+++ companies/views/permissions.py
@@ -1,3 +1,13 @@
 from companies.views.base import Base
 from companies.utils.permissions import GroupsPermission
 from companies.serializers import PermissionsSerializer
+
+from rest_framework.response import Response
+
+from django.contrib.auth.models import Permission
+
+class PermissionsDetail(Base):
+    permission_classes = [GroupsPermission]
+
+    def get(self, request):
+       ...
